95e4ae66cd55d660e777e872e7d6b6689bb34707
CespGonzaDev <d.cespedes.3@estudiantec.cr>
2025-11-14 15:59:37 -0600

    Refactor Clicker class: reorder import statement for consistency

--- src/main/java/games/clicker/Clicker.java
+++ src/main/java/games/clicker/Clicker.java
@@ -3,10 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
  
-import model.IGameFunction;
- 
 package games.clicker;
-
+import model.IGameFunction;
 /**
  *
  * @author danie
