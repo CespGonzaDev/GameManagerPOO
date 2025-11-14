091e82289f26d47349519de6443c65a6b9a400ec
CespGonzaDev <d.cespedes.3@estudiantec.cr>
2025-11-14 15:59:08 -0600

    Initialize gameManager project with basic structure and classes

---
+++ src/main/java/games/tictactoe/Tictactoe.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package games.tictactoe;
+import model.*;
+/**
+ *
+ * @author danie
+ */
+public class Tictactoe implements IGameFunction{
+    void iniciar(){
+        ///arranque
+    }
+    Stat getStats(){
+        return null;
+    }
+
+    void setGameListener(IGameListener listener) {
+        // handle or store the listener; no-op for now
+    }
+
+}
